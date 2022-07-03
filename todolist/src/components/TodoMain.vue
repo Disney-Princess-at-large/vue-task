ea587970a0f61dbcb0e268efa1301ecffeb947bc
grj <3303272844@qq.com>
2022-07-03 21:28:46 +0800

    todolist案例完成

---
+++ todolist/src/components/TodoMain.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="todo-list">
+    <!-- completed: 完成的类名 -->
+    <li :class="{completed:obj.isDone}"  v-for="obj in arr" :key="obj.id">
+      <div class="view">
+        <input class="toggle" type="checkbox" v-model="obj.isDone"/>
+        <label>{{obj.name}}</label>
+        <button class="destroy" @click="delFn(obj.id)"></button>
+      </div>
+    </li>
+  </ul>
+  
+</template>
+
+<script>
+export default {
+  props:{
+    arr:{
+      type:Array,
+      default:()=>[]
+    }
+  },
+  methods:{
+    delFn(id){
+    this.$emit("del",id)
+    }
+  }
+}
+</script>
