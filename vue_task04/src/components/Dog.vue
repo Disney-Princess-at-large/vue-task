6b55119c7fd961e1810f41450d83580bba1cf1f7
Li <1627843848@qq.com>
2022-07-03 19:15:51 +0800

    完成了Dog组件案例

---
+++ vue_task04/src/components/Dog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="box">
+    <img :src="src" alt="">
+    <p>这是一条Dog</p>
+  </div>
+</template>
+
+<script>
+import imgSrc from '@/123.jpg'
+export default {
+    data() {
+        return {
+            src: imgSrc
+        }
+    },
+}
+</script>
+
+<style scoped>
+.box{
+    width: 300px;
+    height: 350px;
+    border: 1px solid #000;
+    display: inline-block;
+}
+p{
+    text-align: center;
+}
+img{
+    width: 300px;
+}
+</style>
