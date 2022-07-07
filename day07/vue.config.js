f707075f6a715b0680297c89fc9bcd346423a553
grj <3303272844@qq.com>
2022-07-07 17:34:31 +0800

    自定义指令的练习

--- day07/vue.config.js
+++ day07/vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+/* 覆盖webpack的配置 */
+// vue脚手架项目--默认配置文件
+module.exports = {
+  devServer: {
+    port: 3000,
+    open: true, //浏览器自动打开
+  },
+  lintOnSave: false, //关闭eslint检查
+};
