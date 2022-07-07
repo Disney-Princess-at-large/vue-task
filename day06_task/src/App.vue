4e2998e47e8728c3bb531266237872d583cc8e6c
Li <1627843848@qq.com>
2022-07-07 17:07:49 +0800

    day07task初始化

--- day06_task/src/App.vue
+++ day06_task/src/App.vue
@@ -1,68 +1,63 @@
 <template>
-  <div>
-     <MyTable :arr="list">
-      <template v-slot:Name="scope">
-    <input v-model="scope.row.name" type="">
-    </template>
-    <template v-slot:age="scope">
-    <input v-model="scope.row.age" type="">
-    </template>
-    <template v-slot:toux="scope">
-       <span v-if="scope.row.type ==2">{{ scope.row.headImgUrl }}</span>
-       <span v-else-if="scope.row.type ==0"><a :href="scope.row.headImgUrl">{{ scope.row.headImgUrl }}</a></span>
-       <span v-else><img style="width: 70px;" :src="scope.row.headImgUrl" alt=""></span>
-     </template>
-    </MyTable>
+  <div class="main">
+    <button @click="comName = 'Phone'">手机注册</button>
+    <button @click="comName = 'Email'">邮箱注册</button>
+
+    <keep-alive>
+      <component :is="comName">
+        <template v-slot:phone>
+          <el-input
+            v-model="input"
+            placeholder="请输入手机号码"
+            class="box"
+          ></el-input>
+        </template>
+        <template v-slot:email>
+          <el-input
+            v-model="input"
+            placeholder="请输入邮箱号"
+            class="box"
+          ></el-input>
+        </template>
+      </component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import MyTable from "./components/MyTable.vue"
+import Phone from '@/components/phone.vue'
+import Email from '@/components/Email.vue'
 export default {
-data() {
-  return {
-    list: [
-        {
-          id: 100,
-          name: "小传同学",
-          age: 18,
-          headImgUrl:
-            "http://yun.itheima.com/Upload/./Images/20210303/603f2d2153241.jpg",
-          type: 0, // 0 显示成a标签
-        },
-        {
-          id: 101,
-          name: "小黑同学",
-          age: 25,
-          headImgUrl:
-            "http://yun.itheima.com/Upload/./Images/20210304/6040b101a18ef.jpg",
-          type: 1, // 1 显示成图片
-        },
-        {
-          id: 102,
-          name: "智慧同学",
-          age: 21,
-          headImgUrl:
-            "http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg",
-          type: 1, // 1 显示成图片
-        },
-        {
-          id: 103,
-          name: "智慧同学",
-          age: 21,
-          headImgUrl:
-            "http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg",
-          type: 2, // 2 显示成文本
-        },
-      ],
-  }
-},
-components:{
-  MyTable,
-}
+  data() {
+    return {
+      activeName: 'second',
+      comName: 'Phone',
+    }
+  },
+  components: {
+    Phone,
+    Email,
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    },
+  },
 }
 </script>
 
 <style>
-
+body {
+  background-color: #ccc;
+}
+.main {
+  width: 400px;
+  height: 600px;
+  background-color: #fff;
+  margin: 20px auto;
+}
+.left{
+  position: relative;
+  rigth:0px;
+}
 </style>
